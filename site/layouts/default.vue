83d0a45a3ecda895566b6f2eaf7400f8693b569d
takasqr <95556025+takasqr@users.noreply.github.com>
2024-08-20 05:30:14 +0900

    chore: add landing page (#9)

---
+++ site/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <slot />
+  <Footer></Footer>
+</template>
+
+<script setup lang="ts">
+import Footer from '@takasqr/tw-vue-ui/custom/general/CustomFooter.vue'
+</script>
---
+++ site/nuxt.config.ts
@@ -0,0 +1,37 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/content",
+    '@nuxtjs/i18n'
+  ],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        iso: 'en-US'
+      },
+      {
+        code: 'ja',
+        iso: 'ja-JP'
+      },
+      {
+        code: 'es',
+        iso: 'es-ES'
+      },
+      {
+        code: 'fr',
+        iso: 'fr-FR'
+      }
+    ],
+    strategy: "no_prefix",
+    vueI18n: './i18n.config.ts'
+  }
+})
